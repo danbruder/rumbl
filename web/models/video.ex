00485e2ce71cc5bb5a6516244357cf30725489b1
Dan Bruder <dbruder@rapiddg.com>
2017-09-20 07:40:54 -0400

    testing

--- web/models/video.ex
+++ web/models/video.ex
@@ -6,16 +6,21 @@ defmodule Rumbl.Video do
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User, foreign_key: :user_id
+    belongs_to :category, Rumbl.Category
 
     timestamps()
   end
+
+  @required_fields ~w(url title description)
+  @optional_fields ~w(category_id)
 
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
+    |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
   end
 end
